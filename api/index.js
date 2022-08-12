2051c5137320182f0739668e0c64647534cb41e8
Todd Rylaarsdam <todd@toddr.org>
2022-08-12 12:44:26 -0500

    Remove express app redirecting to built vue app

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, "..", "dist")));
+// app.use(express.static(path.join(__dirname, "..", "dist")));
 app.use(require("./apiAuth"))
 app.use("/api/v1", require("./routes/v1"))
 app.use("/api", require("./routes/v1")) // routes to latest API version
@@ -14,6 +14,6 @@ app.listen(6001, () => {
   console.log("support system started on port 6001");
 });
 
-app.use((req, res, next) => { //send all other requests to react app
-  res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
-});
+// app.use((req, res, next) => { //send all other requests to react app
+//   res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
+// });
